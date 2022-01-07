f815988164d1511c815ae7c4c03499995414eb24
mudiT025 <muditjain025@gmail.com>
2022-01-07 12:57:23 +0530

    hangman game

---
+++ hangman.py
@@ -0,0 +1,49 @@
+import random
+import requests                                              # module to take data from api
+url="https://random-word-api.herokuapp.com/word?number=1"
+response=requests.get(url)
+given_word = (response.json()[0])
+
+print(" Lets play Hangman ".center(150,'*'))
+
+hint=random.choice(given_word)+""+random.choice(given_word)
+
+
+for char in given_word:    #iterate
+    if char in hint :
+        print(char,end=" ")
+    else :
+    
+        print("_",end=" ")
+
+
+    
+
+
+
+guessed = ""+hint
+lives = 5
+while lives>0:
+        guess = input(" enter a letter you gussed: ")
+        if  guess in given_word :
+           print(guess, "in given word")
+        else :
+           lives -=1
+           print("your letter", guess,"not in given word")
+           print("lives left = ", lives)
+        guessed = guessed + guess
+        wrong_guess = 0
+        for char in given_word:
+            if char in guessed:
+                 print(char , end="")
+            else:
+                 print("-",end="")
+                 
+                 wrong_guess += 1
+        if wrong_guess == 0: 
+            print (" is your word  congratulation  ")
+            break
+else :
+    print("\nbetter luck next time") 
+    print (given_word ," is your word ")      
+    
